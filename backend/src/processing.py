79afb5a35038325b8dcbf41cc9a91cd78b70477f
Sergei Maksimov <maksimov@self.team>
2025-02-02 13:31:27 +0700

    add more examples to the prompts

--- backend/src/processing.py
+++ backend/src/processing.py
@@ -21,19 +21,19 @@ client = AsyncOpenAI(api_key=OPENAI_API_KEY, http_client=http_client)
 
 # Asynchronous function to process text with OpenAI API
 async def extract_pairs_from_text(text: str):
-    return [{"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."}]
+    # return [{"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+            # {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."}]
     prompt = (
         "You are a perfect English teacher. Teacher are very helpful. Teacher helps me to learn English. "
         "I use Anki cards to learn English and I need teacher help. My main language is Russian. "
@@ -44,6 +44,8 @@ async def extract_pairs_from_text(text: str):
         "For each item, teacher provides a pair consisting of the original text and either a definition in English or a translation if it's complex. "
         "Teacher should make cards understandeble for intermediate level. "
         "Teacher must put collocations and words in the context to make it more learnable. "
+        "Teacher must put collocations in the exactly one context: one collocation - one card."
+        "Teacher, please, make context acording to my life: I'm about IT, machine learning team lead, hiking, running, cooking, peace, education."
         "Teacher should only include items that are useful for language learning. Here are some examples:\n\n"
         "Example 1:\n"
         "Text: 'She has a knack for languages.'\n"
@@ -51,8 +53,8 @@ async def extract_pairs_from_text(text: str):
         "Example 2:\n"
         "Text: 'The enigmatic smile of the Mona Lisa has intrigued people for centuries.'\n"
         "Cards: [\n"
-        "{'Front': 'enigmatic smile of the Mona Lisa', 'Back': 'A mysterious or puzzling the Mona Lisa smile'},\n"
-        "{'Front': 'this smile has intrigued people', 'Back': 'This smile has interested people deeply.'}\n"
+        "  {'Front': 'enigmatic smile of the Mona Lisa', 'Back': 'A mysterious or puzzling the Mona Lisa smile'},\n"
+        "  {'Front': 'this smile has intrigued people', 'Back': 'This smile has interested people deeply.'}\n"
         "]\n\n"
         "Example 3:\n"
         "Text: 'custom - sth that people in society or a community usually do: It's a custom for people to give presents to a couple getting married.'\n"
@@ -69,6 +71,13 @@ async def extract_pairs_from_text(text: str):
         "Example 7:\n"
         "Text: 'to be worth sth'\n"
         "Cards: [{'Front': 'to be worth sth - having a particular amount of money', 'Back': 'She must be worth at least half a million.'}]\n\n"
+        "Example 8:\n"
+        "Text: 'find a way'\n"
+        "Cards: [{'Front': 'find a way - to discover how to achieve or deal with something', 'Back': 'Finding a way through the legislation is impossible without expert advice.'}]\n\n"
+        "Example 9:\n"
+        "Text: 'be/get carried away'\n"
+        "Cards: [{'Front': 'be/get carried away - to be so excited about something that you cannot control what you say or do', 'Back': 'There’s far too much food – I’m afraid I got carried away!'}]\n\n"
+        "Keep in mind format cards as: {'Front': 'collocation - definition', 'Back': 'simple example'}"
         "Now, extract cards from the following text:\n"
         )
     try:
@@ -133,20 +142,20 @@ async def extract_pairs_from_text(text: str):
     
 # Asynchronous function to process image with OpenAI API
 async def extract_pairs_from_image(base64_image, image_caption=""):    
-    return [{"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."},
-            {"Front": "knack for", "Back": "An aptitude for doing something."}]
+    # return [{"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."},
+    #         {"Front": "knack for", "Back": "An aptitude for doing something."}]
     """Send an image and caption to OpenAI for structured processing and return extracted information."""
     try:
         user_content = [
@@ -155,13 +164,15 @@ async def extract_pairs_from_image(base64_image, image_caption=""):
                 "text": (
                     "You are a perfect English teacher. Teacher are very helpful. Teacher helps me to learn English. "
                     "I use Anki cards to learn English and I need teacher help. My main language is Russian. "
-                    "My current English level is about B1. Teacher goals is to help me to learn with anki. "
+                    "My current English level is about B1-B2. Teacher goals is to help me to learn with anki. "
                     "I want teacher to help me to learn English like natives, without russian dialects. "
                     "For this goal teacher helps me to create anki cards with collocations, phrazes, sentences, etc - everything for effective learning. "
                     "Teacher gets as an input image with text on it and extracts any cards pairs of words and their translations to help me improve my English and speak like natives. "
                     "For each item, teacher provides a pair consisting of the original text and either a definition in English or a translation if it's complex. "
                     "Teacher should make cards understandeble for intermediate level. "
                     "Teacher must put collocations and words in the context to make it more learnable. "
+                    "Teacher must put collocations in the exactly one context: one collocation - one card."
+                    "Teacher, please, make context acording to my life: I'm about IT, machine learning team lead, hiking, running, cooking, peace, education."
                     "Teacher should only include items that are useful for language learning. Here are some examples:\n\n"
                     "Example 1:\n"
                     "Text: 'She has a knack for languages.'\n"
@@ -187,7 +198,14 @@ async def extract_pairs_from_image(base64_image, image_caption=""):
                     "Example 7:\n"
                     "Text: 'to be worth sth'\n"
                     "Cards: [{'Front': 'to be worth sth - having a particular amount of money', 'Back': 'She must be worth at least half a million.'}]\n\n"
-                    "Now, extract cards from the following text:\n"
+                    "Example 8:\n"
+                    "Text: 'find a way'\n"
+                    "Cards: [{'Front': 'find a way - to discover how to achieve or deal with something', 'Back': 'Finding a way through the legislation is impossible without expert advice.'}]\n\n"
+                    "Example 9:\n"
+                    "Text: 'be/get carried away'\n"
+                    "Cards: [{'Front': 'be/get carried away - to be so excited about something that you cannot control what you say or do', 'Back': 'There’s far too much food – I’m afraid I got carried away!'}]\n\n"
+                    "Keep in mind format cards as: {'Front': 'collocation - definition', 'Back': 'simple example'}"
+                    "Now, extract cards from the following images:\n"
                 ),
             },
             {"type": "text", "text": "Image caption: " + image_caption},
